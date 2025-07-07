7a130279b76b703531ea942181e8aca0a0aa787f
theshuabhmy <theshubham.dev@gmail.com>
2025-07-07 15:51:39 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Shubham kumar on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoTests/TodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTests.swift
+//  TodoTests
+//
+//  Created by Shubham kumar on 07/07/25.
+//
+
+import Testing
+@testable import Todo
+
+struct TodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
